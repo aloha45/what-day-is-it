1478f32c2c19ef01c67b670f127f55e1a954b8cc
Cory <corys4139@gmail.com>
2024-01-05 12:24:53 -0500

    add confetti from ben. thanks ben.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # what-day-is-it
 
 This program will tell you what day of the week it is based on the date input.
+
+Special thanks to Ben Manley for the confetti. 
--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-const submitBtn = document.getElementById('checkDay');
-const resetBtn = document.getElementById('reset');
-const text = document.getElementById('textDiv');
-const container = document.getElementById('containerDiv');
-const img = document.getElementById('imgDiv')
+// event listeners
+const submitBtn = document.getElementById("checkDay");
+const resetBtn = document.getElementById("reset");
+const text = document.getElementById("textDiv");
+const container = document.getElementById("containerDiv");
+const img = document.getElementById("imgDiv");
 
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const daysOfWeek = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
 
-text.innerHTML="";
-container.innerHTML="";
-img.innerHTML="";
+text.innerHTML = "";
+container.innerHTML = "";
+img.innerHTML = "";
 
-submitBtn.addEventListener('click', ()=> {
-    today = new Date()
-    dayToday = today.getDay()
-    container.innerHTML=(daysOfWeek[dayToday])
-    if (dayToday === 5) {
-        text.innerText="Yes. It is:";
-        img.innerHTML='<img class="reaction" src="/images/yes.jpg">';
-        confetti.start();  
-    } else {
-        text.innerText="No. It is:";
-        img.innerHTML='<img class="reaction" src="/images/no.jpg">';
-    }
+submitBtn.addEventListener("click", () => {
+	today = new Date();
+	dayToday = today.getDay();
+	container.innerHTML = daysOfWeek[dayToday];
+	if (dayToday === 5) {
+		text.innerText = "Yes. It is:";
+		img.innerHTML = '<img class="reaction" src="/images/yes.jpg">';
+        confetti.start(2000, ["red", "#FFFFFF", "cornflowerblue"]);
+	} else {
+		text.innerText = "No. It is:";
+		img.innerHTML = '<img class="reaction" src="/images/no.jpg">';
+	}
 });
 
-resetBtn.addEventListener('click', () => {
-    text.innerText="";
-    container.innerHTML="";
-    img.innerHTML="";
-    confetti.stop();
-})
+resetBtn.addEventListener("click", () => {
+	text.innerText = "";
+	container.innerHTML = "";
+	img.innerHTML = "";
+	confetti.stop();
+});
